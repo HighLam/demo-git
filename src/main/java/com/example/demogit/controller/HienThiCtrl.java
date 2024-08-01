1607223b31d5dba20ed4f0d5ff2cfa8a742cabd6
High Lam <hhighlam2004@gmail.com>
2024-08-02 03:34:13 +0700

    add

---
+++ src/main/java/com/example/demogit/controller/HienThiCtrl.java
@@ -0,0 +1,10 @@
+package com.example.demogit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HienThiCtrl {
+    public String hienThiCtrl() {
+        return "hienThiCtrl";
+    }
+}
